c31a3cb9b0cefefa6ca5876457d8e99e8b2245e7
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-02 11:01:17 +0530

    refactor: modify website layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import './App.css';
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+    let userName = "Sumanta";
+    return(
+        <>
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+        </nav>
+            <div className="blank">
+                <p>It's time to code in react</p>
+            </div>
+            <div className="container">
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Hello My name is : <a href="https://github.com/sumanta-k" target="_blank">{userName}</a>
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React With Sumanta
-                </a>
-            </header>
-        </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eaque vero adipisci aspernatur tempore error deserunt? Asperiores sit accusantium non minima facere quia aspernatur! Exercitationem reiciendis doloribus alias quidem amet.
+                </p>
+            </div>
+            <footer>
+                <li><a href='https://github.com/sumanta-k' target="_blank">github</a></li>
+            </footer>
+        </>
     );
 }
 
