6a0bac8bfc67c64d128077bb50a91a2a86577048
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-04 08:05:12 +0530

    feat: add navbar to website.
    Import from the bootstrap, remove some unnecessity links from it.
    For future use-cases search bar isn't deleted.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
-import './App.css';
+import "./App.css";
 function App() {
-    let userName = "Sumanta";
-    return(
+    return (
         <>
-        <nav>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Projects</a></li>
-        </nav>
-            <div className="blank">
-                <p>It's time to code in react</p>
-            </div>
-            <div className="container">
-                <p>
-                    Hello My name is : <a href="https://github.com/sumanta-k" target="_blank">{userName}</a>
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eaque vero adipisci aspernatur tempore error deserunt? Asperiores sit accusantium non minima facere quia aspernatur! Exercitationem reiciendis doloribus alias quidem amet.
-                </p>
-            </div>
-            <footer>
-                <li><a href='https://github.com/sumanta-k' target="_blank">github</a></li>
-            </footer>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        TextUtil
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    href="/"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/">
+                                    About
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input
+                                className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button
+                                className="btn btn-outline-success"
+                                type="submit"
+                            >
+                                Search
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }
