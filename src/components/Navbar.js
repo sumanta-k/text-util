bc52588f5f5f566dbf003edc4bdf265020457d5e
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-04 10:45:05 +0530

    refactor: about with

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar(props) {
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" href="/">
-                                About
+                                {props.about}
                             </a>
                         </li>
                         <li className="nav-item">
