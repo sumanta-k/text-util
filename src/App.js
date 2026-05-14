0959b7fc04a34aabbd3fd8fee1406ad4cd4a9cd5
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-14 19:32:04 +0530

    feat: Add light mode as default
    create toggleMode function which will led to decide which mode to pick
    It could change dark to light and light to dark.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Navbar from "./components/Navbar.js";
 import TextForm from "./components/TextForm.js";
 import About from "./components/About.js";
 function App() {
+    const [mode, setMode] = useState("light"); // Whether dark mode is enabled or NOT
+    const toggleMode = () => {
+        if (mode === "light") {
+            setMode("dark");
+            document.body.style.backgroundColor = "grey";
+        } else {
+            setMode("light");
+            document.body.style.backgroundColor = "white";
+        }
+    };
     return (
         <>
             <Navbar title="TextUtils" about="About" />
