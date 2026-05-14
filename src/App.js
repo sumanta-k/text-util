c38e0a4f3bfe9549048d3e7e231f8cb3818e970c
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-14 13:40:44 +0530

    refactor: add a class and modify the old props value to About

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar.js";
 import TextForm from "./components/TextForm.js";
+import About from "./components/About.js";
 function App() {
     return (
         <>
-            <Navbar title="TextUtils" about="about" />
-            <div className="container">
+            <Navbar title="TextUtils" about="About" />
+            <div className="container my-3">
                 <TextForm
                     heading="Put Text Over Here"
                     title="Text To Analyze"
                 />
+                {/* <About /> */}
             </div>
         </>
     );
