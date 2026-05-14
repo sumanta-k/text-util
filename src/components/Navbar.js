c5d459b898ea41365cedaa762de8ae3ab87dc99e
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-14 19:42:07 +0530

    chore: fix indentation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,9 +36,23 @@ export default function Navbar(props) {
                             Enable Dark Mode
                         </label>
                     </div>
+                    <form className="d-flex" role="search">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button
+                            className="btn btn-outline-success"
+                            type="submit"
+                        >
+                            Search
+                        </button>
+                    </form>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 }
 Navbar.propTypes = {
