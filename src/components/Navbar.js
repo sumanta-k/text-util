ec06ed947c9ba6ae695ba4ab01fed0a5e0f1025c
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-14 19:41:03 +0530

    feat(background-color): toggle button which will decide to change bg-dark or bg-ligh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,44 +20,21 @@ export default function Navbar(props) {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div
-                        className="collapse navbar-collapse"
-                        id="navbarSupportedContent"
+                        className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"}`}
                     >
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link active"
-                                    aria-current="page"
-                                    href="/"
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/">
-                                    {props.about}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/">
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-                        <form className="d-flex" role="search">
-                            <input
-                                className="form-control me-2"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            />
-                            <button
-                                className="btn btn-outline-success"
-                                type="submit"
-                            >
-                                Search
-                            </button>
-                        </form>
+                        <input
+                            onClick={props.toggleMode}
+                            className="form-check-input"
+                            type="checkbox"
+                            role="switch"
+                            id="switchCheckDefault"
+                        />
+                        <label
+                            className="form-check-label"
+                            htmlFor="switchCheckDefault"
+                        >
+                            Enable Dark Mode
+                        </label>
                     </div>
                 </div>
             </nav>
