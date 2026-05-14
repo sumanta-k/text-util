0391a598540cd1de433c17d85c44034485973836
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-05-14 19:39:03 +0530

    feat: change navbar and body color based on the mode (dark/light)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function Navbar(props) {
     return (
         <div className="container">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
                         {props.title}
